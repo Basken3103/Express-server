2bdb1922edf88edf97b8fd707777ce908e100a21
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-11-28 12:22:19 +0100

    Min færdige express server

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import express from 'express';
+import { readFileSync } from 'fs';
 
 const app = express();
 
 const PORT = 3000;
+
+const errorImage = readFileSync('./images/no-coffee.svg');
 
 app.get('/api', (request, response) => {
     response.send('Hey girl!')
 }); 
 
 app.get('/error', (request, response) => {
-    response.status(418).send();
+    response
+    .status(418)
+    .type('image/svg+xml')
+    .send(errorImage);
 });
 
 app.listen(PORT, () => {
